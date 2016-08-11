f2dedd9f993e23618d704cae486a9c6dc1634f84
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2016-08-11 23:19:17 +0200

    😄 Fetch render analytics data in frontend
    - Parse no. of posts
    - Parse no. of views
    
    Fetch from fribase and parse it serverside, then listen to firebase with js to have the modern reactive feel
    
    Resolves frontend requirements for #6

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 require_once 'firebase.php';
-$viewCount = firebase([], 'analytics/page_views', 'GET');
+$analytics = firebase([], 'analytics', 'GET');
+if (!empty($analytics))
+{
+  $viewCount = $analytics['page_views'];
+  $postCount = $analytics['post_count'];
+}
+else
+{
+  $viewCount = 1;
+  $postCount = 0;
+}
 firebase(['page_views' => ++$viewCount], 'analytics', 'PATCH');
 ?>
 <!doctype html>
@@ -17,9 +27,9 @@ firebase(['page_views' => ++$viewCount], 'analytics', 'PATCH');
 <body>
   <div id="topBar">
     <div class="wrapper">
-      <span class="pt-m left">Posts: {{ posts }}</span>
+      <span class="pt-m left">Posts: <?=$postCount?></span>
       <button class="mt-m">export</button>
-      <span class="pt-m right">Views: {{ views }}</span>
+      <span class="pt-m right">Views: <?=$viewCount?></span>
     </div>
   </div>
 
@@ -42,23 +52,36 @@ firebase(['page_views' => ++$viewCount], 'analytics', 'PATCH');
   </div>
 
   <script>
-    var uploadElem = document.querySelector('input[type=file]');
-    uploadElem.onchange = function () {
-      uploadElem.form.submit();
-    }
+
   </script>
   <script src="https://www.gstatic.com/firebasejs/3.2.1/firebase.js"></script>
   <script>
+    var uploadElem = document.querySelector('input[type=file]');
+    uploadElem.onchange = function () {
+      uploadElem.form.submit();
+    };
+
     // Initialize Firebase
     var config = {
       apiKey: "AIzaSyCtPjk3502PFHUW0-SvbUWE7-qZ56Nn6Qw",
       authDomain: "woepla-727f7.firebaseapp.com",
       databaseURL: "https://woepla-727f7.firebaseio.com",
-      storageBucket: "woepla-727f7.appspot.com",
+      storageBucket: "woepla-727f7.appspot.com"
     };
     firebase.initializeApp(config);
 
     var database = firebase.database();
+    var analytics = database.ref('analytics');
+
+    analytics.on('value', function (snap) {
+      var newSnap = snap.val();
+
+      var postsElem = document.querySelector('#topBar span:first-child');
+      var viewsElem = document.querySelector('#topBar span:last-child');
+
+      postsElem.innerText = postsElem.innerText.replace(/\d+/, newSnap.post_count);
+      viewsElem.innerText = viewsElem.innerText.replace(/\d+/, newSnap.page_views);
+    })
   </script>
 </body>
 </html>
