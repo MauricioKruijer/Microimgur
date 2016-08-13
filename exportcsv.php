d283451668e5fc58ba82b14fae8371f02b6648d5
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2016-08-13 14:56:42 +0200

    🐫 Download file pressing export button
    Fixes #8

--- exportcsv.php
+++ exportcsv.php
@@ -2,21 +2,33 @@
 require_once 'helpers.php';
 require_once 'firebase.php';
 
-$file  = sprintf('./exports/%s.csv', randomKeys(10, 'int'));
-$fp    = fopen($file, 'w');
-$posts = firebase([], 'files', 'GET', ['orderBy' => '"timestamp"']);
+$filename = randomKeys(10, 'int') . '.csv';
+$file     = sprintf('./exports/%s', $filename);
+$fp       = fopen($file, 'w');
+$posts    = firebase([], 'files', 'GET', ['orderBy' => '"timestamp"']);
+
+fputcsv($fp, ['id', 'url', 'title'], ';');
 
 if (!empty($posts))
 {
   $posts = array_reverse($posts);
 
-  fputcsv($fp, ['id', 'url', 'title'], ';');
-
   foreach ($posts as $key => $post)
   {
-    $fields = [$key,$post['url'],$post['title']];
+    $fields = [$key, $post['url'], $post['title']];
     fputcsv($fp, $fields, ';');
   }
 
   fclose($fp);
 }
+else
+{
+  fputcsv($fp, ['no', 'files', 'found'], ';');
+}
+
+header("Content-type: text/csv");
+header("Content-Disposition: attachment; filename=" . $filename);
+header("Pragma: no-cache");
+header("Expires: 0");
+
+echo file_get_contents($file);
